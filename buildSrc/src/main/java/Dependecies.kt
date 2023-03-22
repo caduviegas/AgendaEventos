908ed90087dcd06f6a667d934bc5eb93eb24dcaf
Eduardo Viegas <engenedu@hotmail.com>
2023-03-22 10:18:35 -0300

    Adição Bibliotecas

---
+++ buildSrc/src/main/java/Dependecies.kt
@@ -0,0 +1,27 @@
+object Dependecies {
+
+    const val kotlin_ktx = "androidx.core:core-ktx:${Versions.kotlin_ktx}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val room = "androidx.room:room-runtime:${Versions.room}"
+    const val roomTests ="androidx.room:room-testing:${Versions.room}"
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val kapt = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val androidCore = "android.arch.core:core-testing:${Versions.coreTesting}"
+    const val runner = "com.android.support.test:runner:${Versions.runner}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val gsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    const val okhttpLogginInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
+    const val rxjava = "io.reactivex.rxjava2:rxandroid:${Versions.rxjava}"
+    const val rxjavaRetrofitAdapter ="com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    const val picasso = "com.squareup.picasso:picasso:${Versions.picasso}"
+    const val gson = "com.google.code.gson:gson:${Versions.gson}"
+
+
+}
